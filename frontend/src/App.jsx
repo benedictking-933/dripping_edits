26d8c5eca759d615d132c427c40f0d4da780193a
benedictking-933 <abongwabenedict@gmail.com>
2025-11-02 02:19:57 +0100

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,6 @@
 import React from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Routes, Route } from "react-router-dom";
-import ForgotPasswordForm from './Pages/ForgotPassword';
-import ResetPasswordForm from './Pages/ResetPassword';
 import PaymentForm from './Pages/Payment';
 import AboutUsPage from './Pages/AboutUs';
 import PaymentSuccess from './pages/PaymentSuccess';
@@ -22,10 +20,6 @@ function App() {
         
         {/* Home */}
         <Route path="/" element={<HomePage />} />
-
-        {/* Auth */}
-        <Route path="/forgot-password" element={<ForgotPasswordForm />} />
-        <Route path="/reset-password" element={<ResetPasswordForm />} />
 
         {/* Payment & Info */}
         <Route path="/payment" element={<PaymentForm />} />
